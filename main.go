0a8990cdbea2b46b1f51b3ff9c4580ac5b0de3ee
Yan Selivanov <yan.selivanov@pm.by>
2022-04-11 17:34:19 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+const PassStatus = "pass"
+const FailStatus = "fail"
+
+type HealthCheck struct {
+	ServiceID string
+	Status    string
+}
+
+func GenerateCheck() HealthCheck {
+	return HealthCheck{
+		ServiceID: "",
+		Status:    PassStatus,
+	}
+}
+
+func main() {
+	fmt.Println("Тут будет выведен идентификатор")
+}
